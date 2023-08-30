6b33b1803adeddd21fa01df2c74ac611188be395
Iren4ik <124435764+Iren4ik@users.noreply.github.com>
2023-08-30 14:49:30 +0300

    fix/ Add wrong path handling

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+const { NotFound } = require('./utils/errors');
+
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Приветики');
+});
 
 app.use(bodyParser.json()); // для собирания JSON-формата
 app.use(bodyParser.urlencoded({ extended: true })); // для приёма веб-страниц внутри POST-запроса
@@ -21,5 +27,9 @@ app.use((req, res, next) => {
 
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
+
+app.use('*', (req, res) => {
+  res.status(NotFound).send({ message: 'Страница не найдена' });
+});
 
 app.listen(PORT);
