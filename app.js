a9a38da6c9ba5a464490fa2460a7bf4c3d7b0623
Iren4ik <124435764+Iren4ik@users.noreply.github.com>
2023-08-31 14:19:56 +0300

    добавлена валидация, улучшены сообщения об ошибках, защита от кибератак

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const helmet = require('helmet');
 
 const { NotFound } = require('./utils/errors');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://localhost:27017/mestodb' } = process.env;
 
 const app = express();
+
+mongoose.connect(DB_URL);
 
 app.get('/', (req, res) => {
   res.send('Приветики');
 });
 
+app.use(helmet());
 app.use(bodyParser.json()); // для собирания JSON-формата
 app.use(bodyParser.urlencoded({ extended: true })); // для приёма веб-страниц внутри POST-запроса
-
-// подключаемся к серверу mongo
-mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use((req, res, next) => {
   req.user = {
